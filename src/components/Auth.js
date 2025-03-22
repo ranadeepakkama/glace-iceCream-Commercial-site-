d06e05f266737acf2eb15668f09a0cabc0c973d7
ranadeepakkama <ranadeepakbunny35@gmail.com>
2025-03-22 14:27:51 +0530

    updated route

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyApY4S9VsHJjoG3QnQI0yZm0Ru5bg30S10",
   authDomain: "auth-1bffd.firebaseapp.com",
   projectId: "auth-1bffd",
-  storageBucket: "auth-1bffd.firebasestorage.app",
+  storageBucket: "auth-1bffd.appspot.com",
   messagingSenderId: "75369454224",
   appId: "1:75369454224:web:9ce19eae6a4543a7319615",
   measurementId: "G-NL9H7HYDZS"
--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,15 +1,3 @@
-<script setup>
-  import MainCartComponent from './MainCartComponent.vue'
-  import AboutComponent from './AboutComponent.vue'
-  import ThirdCartComponent from './ThirdCartComponent.vue'
-  import FlavourComponent from './FlavourComponent.vue'
-  import DetailsComponent from './DetailsConmponent.vue'
-  import BottomComponent from './BottomComponent.vue'
-</script>
-
 <template>
   <Navbar/>
-  <div class="mt-5">
-    
-  </div>
 </template>
